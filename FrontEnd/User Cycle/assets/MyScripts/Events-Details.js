570c96b1a87056c9274670991404d4d6551f5a6f
qusai_sn <qusai_sn@outlook.com>
2024-09-26 07:17:00 +0300

    adding js to the event details and adding id's to the tags

---
+++ FrontEnd/User Cycle/assets/MyScripts/Events-Details.js
@@ -0,0 +1,11 @@
+// Tags for use with id's : 
+
+//  <h2 class="title" id="Ticket_price">$19.00</h2>
+
+//  <span id="Event_Date">26/10/2024</span>
+
+//  <span id="Event_start_time">10.00am</span>
+
+// <span id="Event_end_time">3.00pm</span>
+
+
